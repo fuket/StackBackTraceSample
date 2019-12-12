46493eb62ac4c3c46be99df3202b65981e93fec1
Tomoki Fuke <earth-pathfinder@live.jp>
2019-12-13 06:00:14 +0900

    プロジェクト ファイルを追加します。

---
+++ StackBackTraceSample/StackBackTrace.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <windows.h>
+#include <string>
+
+class StackBackTrace
+{
+public:
+	StackBackTrace();
+	virtual ~StackBackTrace();
+
+	std::string build();
+
+private:
+	static const int MAX_FRAMES_TO_CAPTURE = 256;
+	static const int MAX_SYMBOL_NAME_LEN = MAX_PATH;
+
+	const HANDLE _hProcess;
+	const bool _symInitialized;
+};
+
+
---
+++ StackBackTraceSample/StackBackTraceSample.cpp
@@ -0,0 +1,50 @@
+﻿// StackBackTraceSample.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include "pch.h"
+#include <iostream>
+#include "StackBackTrace.h"
+
+void test2()
+{
+	char *a = nullptr;
+	memcpy(a, "abc", 4);
+}
+
+void test1()
+{
+	test2();
+}
+
+void se_translator(unsigned int u, _EXCEPTION_POINTERS *e)
+{
+	printf_s("In se_translator. cEip = 0x%x\n", e->ContextRecord->Eip);
+	StackBackTrace stackBackTrace;
+	printf_s(stackBackTrace.build().c_str());
+}
+
+int main()
+{
+	_set_se_translator(se_translator);
+
+	try
+	{
+		test1();
+	}
+	catch (...)
+	{
+	}
+
+	return 0;
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
